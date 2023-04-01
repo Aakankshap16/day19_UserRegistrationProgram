3727b9c874c6d06db98613e5feb84ab230531b88
Aakankshap16 <aakankshap971@gmail.com>
2023-04-01 22:16:58 +0530

    check valid first name

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Page!!");
-
+            Console.WriteLine("********************************");
+            UserRegistrationCode obj = new UserRegistrationCode();
+            Console.WriteLine("Enter the First Name:");
+            string FName = Console.ReadLine();
+            Console.WriteLine(obj.ValidateFName(FName));
         }
     }
 }
