9b52efe2d1465fdf85d9a2b140b515d8590d41c6
Aakankshap16 <aakankshap971@gmail.com>
2023-04-01 20:25:23 +0530

    Greeting msg

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistrationProgram
+{
+    class Program
+    {public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Page!!");
+
+        }
+    }
+}
